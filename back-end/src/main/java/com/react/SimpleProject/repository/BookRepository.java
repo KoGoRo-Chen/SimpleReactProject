704ecedd42f0d2188b9310ee90963738fe213cb5
KoGoRo-Chen <kogoro501@gmail.com>
2024-03-11 16:29:10 +0800

    git

---
+++ back-end/src/main/java/com/react/SimpleProject/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package com.react.SimpleProject.repository;
+
+import org.sam.todos.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository  extends JpaRepository<Book, Long> {
+    // This will allow you to perform basic CRUD operations on the Book entity
+}
